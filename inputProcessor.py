4ebf6df92427d08890848babb8f4c0fbd1dbdbbb
hadidabomb2 <hadi.rizvi123@gmail.com>
2019-11-30 16:08:27 +0000

    Adding very basic version of input processing

--- inputProcessor.py
+++ inputProcessor.py
@@ -5,14 +5,17 @@
 
 
 import spacy
+from spacy.symbols import nsubj, VERB
 
 # Load English tokenizer, tagger, parser, NER and word vectors
 nlp = spacy.load("en_core_web_sm")
 
 # Process whole documents
-text = input("QOI: Hi there, how can I help? ")
+# text = input("QOI: Hi there, how can I help? ")
 
-doc = nlp(text)
+doc = nlp("How does Vodafone relate to IBM?")
+arrayOfChunks = [chunk.text for chunk in doc.noun_chunks]
+longestChunk = (max(arrayOfChunks, key=len))
 
 for token in doc:
     print(token.text, token.pos_, token.dep_)
@@ -24,3 +27,41 @@ print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
 # Find named entities, phrases and concepts
 for entity in doc.ents:
     print(entity.text, entity.label_)
+
+# Finding a verb with a subject from below — good
+verbs = set()
+for possible_subject in doc:
+    if possible_subject.dep == nsubj and possible_subject.head.pos == VERB:
+        verbs.add(possible_subject.head)
+print(verbs)
+
+output = set()
+for entity in doc.ents:
+    output.add(entity.text)
+
+for token in doc:
+    if token.pos_=="VERB":
+        if token.dep_!="xcomp":
+                output.add(token.lemma_)
+        
+print(output)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
