f15d4e59725bd8058e3ec227967c493f2a98ea8d
oskar8991 <oskar8991@gmail.com>
2019-11-30 18:37:48 +0000

    gui backup

--- summariser.py
+++ summariser.py
@@ -1,3 +1,70 @@
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+from string import punctuation
+from heapq import nlargest
+
+
+stopwords = list(STOP_WORDS)
+
+def summarize():
+    test1 = inputField.get('1.0', tk.END)
+    test2 = numField.get()
+    print(test1)
+    print(test2)
+
+    document1 ="""Machine learning (ML) is the scientific study of algorithms and statistical models that computer systems use to progressively improve their performance on a specific task. Machine learning algorithms build a mathematical model of sample data, known as "training data", in order to make predictions or decisions without being explicitly programmed to perform the task. Machine learning algorithms are used in the applications of email filtering, detection of network intruders, and computer vision, where it is infeasible to develop an algorithm of specific instructions for performing the task. Machine learning is closely related to computational statistics, which focuses on making predictions using computers. The study of mathematical optimization delivers methods, theory and application domains to the field of machine learning. Data mining is a field of study within machine learning, and focuses on exploratory data analysis through unsupervised learning.In its application across business problems, machine learning is also referred to as predictive analytics."""
+
+    nlp = spacy.load('en_core_web_sm')
+
+    docx = nlp(document1)
+    mytokens = [token.text for token in docx]
+
+    word_frequencies = {}
+    for word in docx:
+        if word.text not in stopwords:
+                if word.text not in word_frequencies.keys():
+                    word_frequencies[word.text] = 1
+                else:
+                    word_frequencies[word.text] += 1
+
+
+    maximum_frequency = max(word_frequencies.values())
+
+    for word in word_frequencies.keys():  
+            word_frequencies[word] = (word_frequencies[word]/maximum_frequency)
+
+    #print(word_frequencies)
+
+    sentence_list = [ sentence for sentence in docx.sents ]
+
+    sentence_scores = {}  
+    for sent in sentence_list:  
+            for word in sent:
+                if word.text.lower() in word_frequencies.keys():
+                    if len(sent.text.split(' ')) < 30:
+                        if sent not in sentence_scores.keys():
+                            sentence_scores[sent] = word_frequencies[word.text.lower()]
+                        else:
+                            sentence_scores[sent] += word_frequencies[word.text.lower()]
+
+    #print(sentence_scores)
+
+    summarized_sentences = nlargest(4, sentence_scores, key=sentence_scores.get)
+
+    #print(summarized_sentences)
+
+    final_sentences = [ w.text for w in summarized_sentences ]
+
+    summary = ' '.join(final_sentences)
+
+    #print("---------")
+    #print(document1)
+    #print("---------")
+    #print(summary)
+
+
+
+
 # GUI
 import tkinter as tk
 from tkinter import *
@@ -19,9 +86,9 @@ tab_control.add(tab2,text=f'{"About":^22}')
 tab_control.pack(expand=1,fill='both')
 
 #'Home' page
-l1 = Label(tab1,text='Welcome to Qoi!', padx=5, pady=5)
+l1 = Label(tab1,text='Welcome to Qoi! Enter a question', padx=5, pady=5)
 l1.grid(row=1,column=0)
-inputField = ScrolledText(tab1, height=12)
+inputField = ScrolledText(tab1, height=3)
 inputField.grid(row=2,column=0,columnspan=2,pady=5,padx=5)
 
 l2 = Label(tab1, text='Number of sentences in summary:',padx=5,pady=5)
@@ -29,70 +96,16 @@ l2.grid(row=4,column=0)
 numField = Entry(tab1)
 numField.grid(row=4,column=1)
 
-b1 = Button(tab1,text='Submit',width=10,bg='#000000',fg='#000000')
+b1 = Button(tab1,text='Submit',width=10,bg='#000000',fg='#000000', command=summarize)
 b1.grid(row=5,column=1,padx=10,pady=10)
 
+l3 = Label(tab1,text='Result', padx=5, pady=5)
+l3.grid(row=6,column=0)
+result = ScrolledText(tab1,height=10)
+result.grid(row=7,column=0,columnspan=3,padx=5,pady=5)
 
 
 
-
-import spacy
-from spacy.lang.en.stop_words import STOP_WORDS
-from string import punctuation
-from heapq import nlargest
-
-
-stopwords = list(STOP_WORDS)
-
-
-document1 = open 
-nlp = spacy.load('en_core_web_sm')
-
-docx = nlp(document1)
-mytokens = [token.text for token in docx]
-
-word_frequencies = {}
-for word in docx:
-    if word.text not in stopwords:
-            if word.text not in word_frequencies.keys():
-                word_frequencies[word.text] = 1
-            else:
-                word_frequencies[word.text] += 1
-
-
-maximum_frequency = max(word_frequencies.values())
-
-for word in word_frequencies.keys():  
-        word_frequencies[word] = (word_frequencies[word]/maximum_frequency)
-
-#print(word_frequencies)
-
-sentence_list = [ sentence for sentence in docx.sents ]
-
-sentence_scores = {}  
-for sent in sentence_list:  
-        for word in sent:
-            if word.text.lower() in word_frequencies.keys():
-                if len(sent.text.split(' ')) < 30:
-                    if sent not in sentence_scores.keys():
-                        sentence_scores[sent] = word_frequencies[word.text.lower()]
-                    else:
-                        sentence_scores[sent] += word_frequencies[word.text.lower()]
-
-#print(sentence_scores)
-
-summarized_sentences = nlargest(4, sentence_scores, key=sentence_scores.get)
-
-#print(summarized_sentences)
-
-final_sentences = [ w.text for w in summarized_sentences ]
-
-summary = ' '.join(final_sentences)
-
-print("---------")
-#print(document1)
-print("---------")
-#print(summary)
 
 
 
