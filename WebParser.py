2bfe44b8d7f66913ef1dbc25d3c0d5850c0bdbe1
oskar8991 <oskar8991@gmail.com>
2019-12-01 09:41:40 +0000

    fixed webparser char limit

--- WebParser.py
+++ WebParser.py
@@ -2,7 +2,10 @@ from bs4 import BeautifulSoup
 from bs4.element import Comment
 import requests
 import urllib.request
-from googlesearch import search  
+from googlesearch import search
+
+# tag_visible() and text_from_html() taken from:
+# https://stackoverflow.com/questions/1936466/beautifulsoup-grab-visible-webpage-text
 
 class WebScrapper:
 
@@ -26,27 +29,40 @@ class WebScrapper:
     def webScrape(self, webList):
         toSummarize = ""
         text = ""
+        print('----------')
+        print(webList)
+        print('----------')
         for x in webList:
-            if "wikipedia" in x:
-                #print('wiki')
-                source = requests.get(x).text
-                soup = BeautifulSoup(source, 'lxml')
-                #print(soup)
-                response = requests.get(x)
-                html = BeautifulSoup(response.text, 'html.parser')
+                if "wikipedia" in x:
+                    #print('wiki')
+                    source = requests.get(x).text
+                    soup = BeautifulSoup(source, 'lxml')
+                    #print(soup)
+                    response = requests.get(x)
+                    html = BeautifulSoup(response.text, 'html.parser')
 
-                title = html.select("#firstHeading")[0].text
-                paragraphs = html.select("p")
-                #print(paragraphs)
-                allText = '\n'.join([para.text for para in paragraphs])
-                toSummarize += " " + allText
-            else:
-                html = urllib.request.urlopen(x).read()
-                #summarise1 = Summarizer()
-                #outputResult = summarise1.summarize(text_from_html(html))
-                #print(outputResult)
-                toSummarize += " " + self.text_from_html(html)
+                    title = html.select("#firstHeading")[0].text
+                    paragraphs = html.select("p")
+                    #print(paragraphs)
+                    allText = '\n'.join([para.text for para in paragraphs])
+                    #toSummarize += " " + allText
+                    for para in paragraphs:
+                        if(len(toSummarize) < 5000):
+                            toSummarize += " " + para.text
+                        else:
+                            break
+                else:
+                    html = urllib.request.urlopen(x).read()
+                    #summarise1 = Summarizer()
+                    #outputResult = summarise1.summarize(text_from_html(html))
+                    #print(outputResult)
+                    #toSummarize += " " + self.text_from_html(html)
+                    if(len(toSummarize) < 5000):
+                        toSummarize += " " + self.text_from_html(html)
+                    else:
+                        break
 
+        print(toSummarize)
         return(toSummarize)
 
     
